529dc5088d02b03424132eb72ee47da97c37618e
juan-rios <juanesrios13@gmail.com>
2020-04-17 18:09:24 -0500

    aa

--- OchoReinas/src/principal/Ocho_Reinas.java
+++ OchoReinas/src/principal/Ocho_Reinas.java
@@ -4,12 +4,24 @@ public class Ocho_Reinas {
 	final static int N=8;
 	static int cont=0;
 	static int [][] tablero=new int[N][N];
-
+	static int [][] soluciones=new int[92][N];
+	static int[] solucionVectorial=new int[8];
+	
 	public static void main(String[] args) {
 		solucionCompleta();	
 	}
+	
+	public static void almacenarSoluciones(int cont) {
+		
+	}
+	
+	public static void solucionVectorial() {
+		
+	}
+	
 	public static void imprimirTablero(){
 		int[] sln=new int[8];
+		
 		for(int i=0;i<tablero.length;i++){
 			for (int j=0;j<tablero.length;j++) {
 				System.out.print(tablero[i][j]+ "\t");
@@ -20,7 +32,7 @@ public class Ocho_Reinas {
 			
 			System.out.println();
 		}
-		System.out.println("solucion vectorial: ");
+		System.out.println("\t\t\tSolucion vectorial: ");
 		print(sln);
 		System.out.println();
 	}
@@ -38,8 +50,8 @@ public class Ocho_Reinas {
 	
 	public static void ponerReina(int col){
 		int fila= 0; // maximo 8 filas disponibles
-		while (fila<N){
-			if (col<N){
+		while(fila<N){
+			if(col<N){
 				if (valida(fila, col)){// Verificar que fila y columna validas
 					tablero[fila][col]=1;
 					ponerReina(col+1);
@@ -49,31 +61,26 @@ public class Ocho_Reinas {
 			fila++;// nueva fila
 		}
 		if (col>=N){
-			cont++;
-			System.out.println("Solucion: "+cont);
+			System.out.println("\t\t\tSolucion: "+(++cont));
 			imprimirTablero();
 			System.out.println();
+			solucionVectorial();
+			almacenarSoluciones(cont);
 		}
 	}
 
-	// Verifica si una posición es válida
-	public static boolean rango(int num){
-		return (num>=0 && num<N);
-	}
-	
 	private static boolean valida(int fila,int col){
 		for (int j = 0; j <= col; j++) {
 			//Verifico que no ocupada la fila
 			if(tablero[fila][j] !=0 && j!=col)
 				return false; 
 			//Verifico diagonal superior
-			if(rango (fila-j)&&rango(col-j) && tablero[fila-j][col-j] != 0 && j>0 )
+			if(fila-j>=0 && col-j>=0 && tablero[fila-j][col-j] != 0 && j>0 )
 				return false;
 			//Verifico diagonal inferior
-			if(rango (fila+j)&& rango(col-j) &&tablero[fila+j][col-j] != 0 && j>0)
+			if(fila+j<N && col-j>=0 &&tablero[fila+j][col-j] != 0 && j>0)
 				return false;	
 		}
-		
 		return true;	
 	}
 }	
